998fe2dc69a047f454616a416fac1919fe25e7ee
Patrick Stuedi <stu@zurich.ibm.com>
2018-04-25 16:55:20 +0200

    reflex draft

--- client/reflex/reflex_message.cc
+++ client/reflex/reflex_message.cc
@@ -4,7 +4,7 @@ ReflexMessage::ReflexMessage(short type, long long ticket, long long lba,
                              int count)
     : type_(type), ticket_(ticket), lba_(lba), count_(count),
       payload_(nullptr) {
-  this->magic_ = sizeof(short) * 2 + sizeof(long long) * +sizeof(count);
+  this->magic_ = sizeof(short) * 2 + sizeof(long long) * 2 + sizeof(count);
 }
 
 ReflexMessage::ReflexMessage(short type, long long ticket, long long lba,
